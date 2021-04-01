b223905e0e8ad706da27947083560b9f6de650a8
SabbanData <sabban@datatrained>
2021-04-01 13:00:23 +0530

    aside bar

---
+++ src/app/Module/aside1/aside1.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Aside1RoutingModule } from './aside1-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, Aside1RoutingModule],
+})
+export class Aside1Module {}
